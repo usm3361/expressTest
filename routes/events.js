6d7e3cdc8ea42e6f3c166779d695648ea75d2338
Uri Miller <usm3361@gmail.com>
2025-12-25 12:03:11 +0200

    added helper function

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createEvent } from "../constractor/events.js";
+import { readEvents } from "../utils/helperFunction.js";
 
 const router = Router();
 
 // routs events
-router.route("/events").post(createEvent);
+router.route("/events").get(readEvents);
+router.route("creator/events").post(createEvent);
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 import { createUser } from "../constractor/users.js";
 import { Router } from "express";
+import { readUsers } from "../utils/helperFunction.js";
 
 const router = Router();
 
 // routs users
-router.route("/users").post(createUser);
+router.route("/users").get(readUsers);
+router.route("/users/register").post(createUser)
+router.route("/users/tickets/buy").post(createUser);
+router.route("/users/:username/summary");
+
+
 
 export default router;
